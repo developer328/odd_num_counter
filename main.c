1f37e4fba849d8bc69e36614ed7d2121af8790f1
developer328 <Nikoloz1F5@gmail.com>
2023-09-24 00:31:30 +0400

    bug fix 0

--- main.c
+++ main.c
@@ -1,20 +1,20 @@
 #include<stdio.h>
 
-//int check_num();
-//void user(void);
+long first_num = 0;
+long last_num = 0;
+long odd_cnt = 0;
+int modulo = 2;
+int res = 0;
+
+void chck_num(long *Pn1,long *Pn2);
 
 
 int main(void)
 {
-	int first_num = 0;
-	int last_num = 0;
-	int modulo = 2;
-	int res = 0;
-	int odd_cnt = 0;
+	char user_want;
 
 	while(1)
-	{
-		char user_want;
+	{	
 		printf("exit = 'q', run = 'r' \n");     
 		scanf("%c", &user_want);
         
@@ -23,85 +23,71 @@ int main(void)
                		 break;
         	}
 		else if(user_want == 'r')
-        	{
-                	continue;
+        	{		
+			printf("min = 0, max = 60000 \n");
+			printf("\n");
+			printf("enter number \n");
+			scanf("%li", &first_num);	
+			printf("enter last number \n");
+			scanf("%li", &last_num);
+		
+			if(first_num < 0)
+			{
+				first_num = 0;
+			}
+			
+			if(first_num > 59990)
+			{
+				first_num = 59990;
+			}
 
+			if(last_num > 60000)
+			{
+				last_num = 60000;
+			}
+			
+			if(last_num < first_num)
+			{
+				printf("error");
+				printf("last num is more than first num");
+				printf("\n");
+	       		}
+			
+			if(last_num > first_num)
+			{
+				odd_cnt = 0;
+				chck_num(&first_num, &last_num);
+				first_num = 0;
+				last_num = 0;
+			}		 
+			user_want = 0;
         	}
-        	else
+		else if(user_want == 0)
+		{
+			;		
+		}
+        	else 
         	{
                 	printf("incorrect symbol \n");
-                	printf("exit = 'q', run = 'r' \n");     
+			printf("\n"); 
+		        printf("\n");	
         	}
-
-
-		printf("min = 0, max = 60000 \n");
-		printf("enter number \n");
-		scanf("%d", &first_num);	
-		printf("enter last number \n");
-		scanf("%d", &last_num);
-		
-		if(first_num < 0)
-		{
-			first_num = 0;
-		}
-		else if(first_num > 59000)
-		{
-			first_num = 59000;
-		}
-
-		if(last_num > 60000)
-		{
-			last_num = 60000;
-		}
-		else if(last_num < first_num)
-		{
-			printf("error");
-			printf("last num is more than first num");
-	        }
-		else if(last_num > first_num)
-		{
-			for(int i = first_num; i < last_num; i++)
-			{
-				res = i % modulo;
-				printf("%d \n", res);
-				if(res == 1)
-				{
-					odd_cnt += 1;
-					printf("odd number = %d \n", i);
-					printf("quantity = %d \n", odd_cnt);
-				}
-			}	
-		}
 	}	
 	return 0;
 }
 
-/*
-int check_num()
+
+void chck_num(long *Pn1, long *Pn2)
 {
-
-}
-
-
-void user(void)
-{	
-	char user_want;
-	printf("exit = 'q', run = 'r' \n");	
-	scanf("%c", &user_want);
-	
-	if(user_want == 'q')
+	for(long i = *Pn1; i < *Pn2; i++)
 	{
-		break;
-	}
-	else if(user_want == 'r')
-	{
-		continue;
-
-	}
-	else
-	{
-		printf("incorrect symbol \n");
-		printf("exit = 'q', run = 'r' \n");	
+		res = i % modulo;
+		printf("%d \n", res);
+		if(res == 1)
+		{
+			odd_cnt += 1;
+			printf("odd number = %li \n", i);
+			printf("quantity = %li \n", odd_cnt);
+		}
 	}
 }
-*/
